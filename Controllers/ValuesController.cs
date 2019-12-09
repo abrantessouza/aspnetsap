ba467aa2567c246849a31456af4b089d4a80b6e5
Thiago Abrantes <thiago.abrantes@interfusao.com.br>
2019-12-09 01:46:17 -0300

    First Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using WebApplication3.Helpers;
+using WebApplication3.ServiceReference1;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+       
+        public string Get()
+        {
+            ZRemotListsample zls = new ZRemotListsample();
+            ZRemotListsampleResponse loadResponse = new ZRemotListsampleResponse();
+
+            try
+            {
+                ZRemotListsampleRequest zr = new ZRemotListsampleRequest();
+                zr.ZRemotListsample = zls;
+                
+               
+                loadResponse = SapConnector.Client().ZRemotListsample(zr.ZRemotListsample);
+               
+             
+                return JsonConvert.SerializeObject(loadResponse);
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return JsonConvert.SerializeObject(loadResponse);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Helpers/SapConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using WebApplication3.ServiceReference1;
+
+namespace WebApplication3.Helpers
+{
+    public static class SapConnector
+    {
+        public static Z_ABRANTES_SERVICESClient Client()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+            EndpointAddress endpoint = new EndpointAddress("http://vhcalnplci:8000/sap/bc/srt/rfc/sap/z_abrantes_services/001/zabrantesservices/abrantesservices");
+            Z_ABRANTES_SERVICESClient client = new Z_ABRANTES_SERVICESClient(binding, endpoint);
+
+            client.ClientCredentials.UserName.UserName = "USER_REST";
+            client.ClientCredentials.UserName.Password = "Test02";
+            return client;
+        }
+    }
+}
